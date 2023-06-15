2dd64df3f2e463bdd38a4f23c9656bc3443331b6
Umer Khokhar <creativeinovators@gmail.com>
2023-06-15 14:18:58 +0500

    -fix local storage

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
-const toDoIn = document.getElementById('to-do-input');
-const toDoItem = document.getElementById('to-do-item');
+const toDoInp = document.getElementById('to-do-input')
+const toDoItem = document.getElementById('to-do-item')
 
-toDoIn.addEventListener('keyup', e => {
+
+// Making the function to make li items dynamically and to remove and done 
+
+let myFunction = (data) => {
+  let listItem = document.createElement('li')
+  listItem.innerHTML = `${data} <img src="remove.png" alt="remove" width="23px">`
+  toDoInp.value = ''
+
+  
+  // To Mark as Done
+  listItem.addEventListener('click', e => {
+    listItem.classList.toggle('done')
+  })
+  
+  //To delete the list item
+  listItem.querySelector('img').addEventListener('click', e => {
+    listItem.remove()
+    localStorageFun() //to call localStorage to remove from local storage also
+  })
+  toDoItem.appendChild(listItem)
+  localStorageFun() // calling the local storage function
+}
+
+toDoInp.addEventListener('keyup', e => {
   if (e.key === "Enter") {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `${toDoIn.value} <img id="remove" src="remove.png" alt="remove" width="23px">`;
-
-    toDoItem.appendChild(listItem);
-    listItem.addEventListener('click', e => {
-      listItem.classList.toggle('done');
-    });
-    toDoIn.value = '';
-
-    listItem.querySelector('img').addEventListener('click', e => {
-      listItem.remove();
-    });
-
-    // Add the item to local storage
-    const todoList = localStorage.getItem('todoList');
-    let todos = [];
-
-    if (todoList) {
-      todos = JSON.parse(todoList);
+    const inputValue = toDoInp.value.trim()
+    if (inputValue !== '') {
+      myFunction(inputValue)
     }
-
-    todos.push(toDoIn.value);
-    localStorage.setItem('todoList', JSON.stringify(todos));
   }
-});
+})
+
+window.addEventListener('load',e => {
+  let myLocalStorage = JSON.parse(localStorage.getItem('myStorage'))
+
+  if (myLocalStorage) {
+    myLocalStorage.forEach(item => {
+      myFunction(item)
+    });
+  }
+})
+
+function localStorageFun() {
+  const listItem = toDoItem.querySelectorAll('li')
+  const items = Array.from(listItem).map(value => {
+    return value.textContent
+    })
+  localStorage.setItem("myStorage",JSON.stringify(items))
+}
