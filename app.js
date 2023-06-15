22e04e9a1a320cb4235c631a762da6d159f5f047
Umer Khokhar <creativeinovators@gmail.com>
2023-06-15 09:56:40 +0500

    -fix local storage

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 const toDoIn = document.getElementById('to-do-input');
-      const toDoItem = document.getElementById('to-do-item');
-      let toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
+const toDoItem = document.getElementById('to-do-item');
 
-      toDoIn.addEventListener('keyup', e => {
-        if (e.key === "Enter") {
-          const listItem = document.createElement('li');
-          listItem.innerHTML = `${toDoIn.value} <img id="remove" src="remove.png" alt="remove" width="23px">`;
+toDoIn.addEventListener('keyup', e => {
+  if (e.key === "Enter") {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${toDoIn.value} <img id="remove" src="remove.png" alt="remove" width="23px">`;
 
-          toDoItem.appendChild(listItem);
-          listItem.addEventListener('click', e => {
-            listItem.classList.toggle('done');
-          });
-          toDoIn.value = '';
+    toDoItem.appendChild(listItem);
+    listItem.addEventListener('click', e => {
+      listItem.classList.toggle('done');
+    });
+    toDoIn.value = '';
 
-          listItem.querySelector('img').addEventListener('click', e => {
-            listItem.remove();
-          });
+    listItem.querySelector('img').addEventListener('click', e => {
+      listItem.remove();
+    });
 
-          // Store the updated to-do list in local storage
-          toDoList.push(toDoIn.value);
-          localStorage.setItem('toDoList', JSON.stringify(toDoList));
-        }
-      });
+    // Add the item to local storage
+    const todoList = localStorage.getItem('todoList');
+    let todos = [];
 
-      // Load the to-do list from local storage on page load
-      toDoList.forEach(item => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `${item} <img id="remove" src="remove.png" alt="remove" width="23px">`;
+    if (todoList) {
+      todos = JSON.parse(todoList);
+    }
 
-        toDoItem.appendChild(listItem);
-        listItem.addEventListener('click', e => {
-          listItem.classList.toggle('done');
-        });
-
-        listItem.querySelector('img').addEventListener('click', e => {
-          listItem.remove();
-        });
-      });
+    todos.push(toDoIn.value);
+    localStorage.setItem('todoList', JSON.stringify(todos));
+  }
+});
