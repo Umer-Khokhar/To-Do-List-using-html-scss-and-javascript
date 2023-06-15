cdcf1315a839fcc1746817cc7693691ef44eacf1
Umer Khokhar <creativeinovators@gmail.com>
2023-06-15 09:18:27 +0500

    -fix local storage

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const toDoIn = document.getElementById('to-do-input')
-const toDoItem = document.getElementById('to-do-item')
+const toDoIn = document.getElementById('to-do-input');
+      const toDoItem = document.getElementById('to-do-item');
+      let toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
 
+      toDoIn.addEventListener('keyup', e => {
+        if (e.key === "Enter") {
+          const listItem = document.createElement('li');
+          listItem.innerHTML = `${toDoIn.value} <img id="remove" src="remove.png" alt="remove" width="23px">`;
 
-toDoIn.addEventListener('keyup', e => {
-    if (e.key === "Enter") {
-        // myFun(toDoIn.value);
-        const listItem = document.createElement('li')
-        listItem.innerHTML = `${toDoIn.value} <img id="remove" src="remove.png" alt="remove" width="23px">`
-        
-        toDoItem.appendChild(listItem)
+          toDoItem.appendChild(listItem);
+          listItem.addEventListener('click', e => {
+            listItem.classList.toggle('done');
+          });
+          toDoIn.value = '';
+
+          listItem.querySelector('img').addEventListener('click', e => {
+            listItem.remove();
+          });
+
+          // Store the updated to-do list in local storage
+          toDoList.push(toDoIn.value);
+          localStorage.setItem('toDoList', JSON.stringify(toDoList));
+        }
+      });
+
+      // Load the to-do list from local storage on page load
+      toDoList.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `${item} <img id="remove" src="remove.png" alt="remove" width="23px">`;
+
+        toDoItem.appendChild(listItem);
         listItem.addEventListener('click', e => {
-            listItem.classList.toggle('done')
-        })
-        toDoIn.value = ''
+          listItem.classList.toggle('done');
+        });
 
         listItem.querySelector('img').addEventListener('click', e => {
-            listItem.remove()
-        } )
-    }
-})
-localStorage.setItem(toDoItem, JSON.stringify(myObj));
-
-// function myFun(item) {
-//     const listItem = document.createElement('li')
-//     listItem.innerHTML = `${item} <img src="remove.png" alt="remove" width="23px">`
-    
-//     toDoItem.appendChild(listItem)
-
-// }
+          listItem.remove();
+        });
+      });
